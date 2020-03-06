938ef06fe00c9613958dad8d9c06b3c3b9c77e18
Karina Yasnovskaya <rina.yasnovskaya@gmail.com>
2020-03-06 17:53:30 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let brackets;
+  let result;
+
+  for(let i = 0; i < bracketsConfig.length; i++){
+    brackets = bracketsConfig[i].join("");
+    
+    if(str.includes(brackets)){
+      str = str.replace(brackets, "");
+      i =- 1;
+    }
+  }
+  
+  if(str.length) result = false;
+  else result = true;
+  
+  return result;
 }
